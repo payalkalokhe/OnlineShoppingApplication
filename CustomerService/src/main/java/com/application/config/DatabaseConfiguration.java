327b3bc3522bf720b56553a0dc8957f8b9763578
Payal Kalokhe <payalkalokhe@gmail.com>
2018-11-15 12:15:05 +0530

    Embedded database configuration

---
+++ CustomerService/src/main/java/com/application/config/DatabaseConfiguration.java
@@ -0,0 +1,18 @@
+package com.application.config;
+
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class DatabaseConfiguration {
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		DataSource dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).setName("customerdb")
+				.addScript("classpath:com/application/config/customer-data.sql").build();
+		return dataSource;
+	}
+}
