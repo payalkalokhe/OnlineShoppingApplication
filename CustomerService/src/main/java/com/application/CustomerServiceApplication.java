b3201ef4dc269830fba4be6014d0eefe036b2967
Payal Kalokhe <payalkalokhe@gmail.com>
2018-12-24 11:45:10 +0530

    New Changes

--- CustomerService/src/main/java/com/application/CustomerServiceApplication.java
+++ CustomerService/src/main/java/com/application/CustomerServiceApplication.java
@@ -3,12 +3,14 @@ package com.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @EnableEurekaClient
 @SpringBootApplication
+@EnableKafka
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
